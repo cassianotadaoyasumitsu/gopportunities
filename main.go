2e978a8f16f85a5f0c0ea605a6dff08b70bb48b9
cassiano yasumitsu <cassiano@usuarioincomum.com>
2024-01-25 00:00:02 +0900

    feat: create logs

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "gopportunities/router"
+import (
+	"gopportunities/config"
+	"gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize the config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing config: %v", err)
+		return
+	}
+
+	// Initialize the router
 	router.Initialize()
 }
