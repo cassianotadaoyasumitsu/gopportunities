3fbca6b6bd0d202f68aebfd71634293fd6fac248
cassiano yasumitsu <cassiano@usuarioincomum.com>
2024-01-27 12:18:36 +0900

    feat: remove comments

--- config/config.go
+++ config/config.go
@@ -6,19 +6,12 @@ import (
 )
 
 var (
-	//db     *gorm.DB
 	dbp    *gorm.DB
 	logger *Logger
 )
 
 func Init() error {
 	var err error
-
-	// Initialize DB
-	//db, err = InitializeSQLite()
-	//if err != nil {
-	//	return fmt.Errorf("error initializing SQLite: %v", err)
-	//}
 
 	dbp, err = InitializePostgres()
 	if err != nil {
@@ -28,9 +21,6 @@ func Init() error {
 	return nil
 }
 
-//	func GETSQLite() *gorm.DB {
-//		return db
-//	}
 func GETPostgres() *gorm.DB {
 	return dbp
 }
--- handler/handler.go
+++ handler/handler.go
@@ -7,12 +7,10 @@ import (
 
 var (
 	logger *config.Logger
-	//db     *gorm.DB
-	dbp *gorm.DB
+	dbp    *gorm.DB
 )
 
 func InitializerHandler() {
 	logger = config.GetLogger("handler")
-	//db = config.GETSQLite()
 	dbp = config.GETPostgres()
 }
