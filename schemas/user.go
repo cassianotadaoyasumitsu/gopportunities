351096bb0342ebb5778efae543c21e70dae4e4b1
cassiano yasumitsu <cassiano@usuarioincomum.com>
2024-01-28 23:27:34 +0900

    feat: remove user info from opening response

--- schemas/user.go
+++ schemas/user.go
@@ -9,14 +9,12 @@ type User struct {
 	gorm.Model
 	Email    string `gorm:"unique"`
 	Password string
-	Openings []Opening `gorm:"foreignkey:UserID"`
 }
 
 type UserResponse struct {
 	ID        uint      `json:"id"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
-	Openings  []Opening `json:"openings"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	DeletedAt time.Time `json:"deleted_at,omitempty"`
